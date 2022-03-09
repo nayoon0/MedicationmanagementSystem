b355dc3b4ec2d8872ddfa6ed7b10a6a8f2318092
nayoon <minso9896@naver.com>
2022-03-09 15:57:45 +0900

    removed hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Meditations");
+		System.out.println("2. Delete Meditations");
+		System.out.println("3. Edit Meditations");
+		System.out.println("4. View Meditations");
+		System.out.println("5. Exit");
+
+	}
+
+}
