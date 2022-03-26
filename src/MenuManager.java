0ad533e0d282a44c53f37e183eee91cfe83c1ead
nayoon <minso9896@naver.com>
2022-03-26 17:48:20 +0900

    add more selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -16,22 +16,59 @@ public class MenuManager {
 			System.out.println("6. Exit");
 			System.out.println("Select one number between 1-6 : ");
 			num = input.nextInt();
-			switch(num) {
-			case 1 : 
-				System.out.print("Medication use");
-				String medicationuse = input.next();
-				System.out.print("Medication name");
-				String medicationname = input.next();
-				System.out.print("Medication price");
-				int medicationprice = input.nextInt();
-				break;
-			case 2 :
-			case 3 :
-			case 4 :
-				System.out.print("Medication name");
-				String medicationname2 = input.next();
+			if (num == 1) {
+				addMedication();
 			}
-			
+			else if (num == 2) {
+				deleteMedication();
 			}
-	   }
+			else if (num == 3) {
+				editMedication();
+			}
+			else if (num == 4) {
+				viewMedication();
+			}
+			else {
+				continue;
+			}
+		}	
 	}
+		public static void addMedication() {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Medication Item Number : ");
+			int medicationIN = input.nextInt();
+			System.out.print("Medication Name : ");
+			String medicationName = input.next();
+			System.out.print("Medication Use: ");
+			String medicationUse = input.next();
+			System.out.print("Medication Price : ");
+			int medicationPrice = input.nextInt();
+			
+	   }
+		public static void deleteMedication() {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Medication Item Number : ");
+			int medicationIN = input.nextInt();
+			System.out.print("Medication Delete Reason : ");
+			String medicationDeleteReason = input.next();
+			
+		}
+
+
+		public static void editMedication() {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Medication Item Number : ");
+			int medicationIN = input.nextInt();
+		
+		
+		}		
+		
+		public static void viewMedication() {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Medication Item Number : ");
+			int medicationIN = input.nextInt();
+			
+		
+		}
+		
+}
