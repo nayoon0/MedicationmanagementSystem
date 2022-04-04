4623f6fe3c1eef6bb0cc03beb4e1ee6512fc8798
nayoon <minso9896@naver.com>
2022-04-04 23:28:40 +0900

    medication Manager3

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,72 +3,35 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		int num = 0;
 		Scanner input = new Scanner(System.in);
-	    
-		while (num != 6) {
+		MedicationManager medicationManager = new MedicationManager(input);
+		int num = -1;
+		
+		while (num != 5) {
 			System.out.println("*** Medications Management System Menu ***");
 			System.out.println("1. Add Medications");
 			System.out.println("2. Delete Medications");
 			System.out.println("3. Edit Medications");
 			System.out.println("4. View Medications");
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit");
+			System.out.println("5. Exit");
 			System.out.println("Select one number between 1-6 : ");
 			num = input.nextInt();
 			if (num == 1) {
-				addMedication();
+				medicationManager.addMedication();
 			}
 			else if (num == 2) {
-				deleteMedication();
+				medicationManager.deleteMedication();
 			}
 			else if (num == 3) {
-				editMedication();
+				medicationManager.editMedication();
 			}
 			else if (num == 4) {
-				viewMedication();
+				medicationManager.viewMedication();
 			}
 			else {
 				continue;
 			}
 		}	
 	}
-		public static void addMedication() {
-			Scanner input = new Scanner(System.in);
-			System.out.print("Medication Item Number : ");
-			int medicationIN = input.nextInt();
-			System.out.print("Medication Name : ");
-			String medicationName = input.next();
-			System.out.print("Medication Use: ");
-			String medicationUse = input.next();
-			System.out.print("Medication Price : ");
-			int medicationPrice = input.nextInt();
-			
-	   }
-		public static void deleteMedication() {
-			Scanner input = new Scanner(System.in);
-			System.out.print("Medication Item Number : ");
-			int medicationIN = input.nextInt();
-			System.out.print("Medication Delete Reason : ");
-			String medicationDeleteReason = input.next();
-			
-		}
-
-
-		public static void editMedication() {
-			Scanner input = new Scanner(System.in);
-			System.out.print("Medication Item Number : ");
-			int medicationIN = input.nextInt();
-		
-		
-		}		
-		
-		public static void viewMedication() {
-			Scanner input = new Scanner(System.in);
-			System.out.print("Medication Item Number : ");
-			int medicationIN = input.nextInt();
-			
-		
-		}
 		
 }
