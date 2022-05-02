06c795a96fe0f2e486dd60a636f4be9dc18097ac
nayoon <minso9896@naver.com>
2022-05-02 12:43:48 +0900

    management system

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MenuManager {
+public class MenuManager { 
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -9,9 +9,9 @@ public class MenuManager {
 		
 		while (num != 5) {
 			System.out.println("*** Medications Management System Menu ***");
-			System.out.println("1. Add Medications");
-			System.out.println("2. Delete Medications");
-			System.out.println("3. Edit Medications");
+			System.out.println("1. Add Medication");
+			System.out.println("2. Delete Medication");
+			System.out.println("3. Edit Medication");
 			System.out.println("4. View Medications");
 			System.out.println("5. Exit");
 			System.out.println("Select one number between 1-6 : ");
@@ -26,7 +26,7 @@ public class MenuManager {
 				medicationManager.editMedication();
 			}
 			else if (num == 4) {
-				medicationManager.viewMedication();
+				medicationManager.viewMedications();
 			}
 			else {
 				continue;
